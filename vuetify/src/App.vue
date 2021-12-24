edfbd69d694ad8225d3ee66b536346565191efbe
Luba Derebas <luba_d2005@mail.ru>
2021-12-24 19:12:44 +0300

    Project without api

--- vuetify/src/App.vue
+++ vuetify/src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Container>
+      <ChatWindow>
+        <ChatMessage :username="username" :time="time">Hello, World!!!</ChatMessage>
+      </ChatWindow>
+    </Container>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Container from './components/Container.vue'
+import ChatMessage from './components/ChatMessage.vue'
+import ChatWindow from './components/ChatWindow.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Container,
+    ChatMessage,
+    ChatWindow
+  },
+  data() {
+    return {
+      username: "Ivan",
+      time: "21.12.2019 05:00:50",
+    };
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  background-color: #f9f9fa;
 }
 </style>
